d0409266b83da40efd978b3a4379f0d703c3fbfb
Dom Smith <dom@teneightfive.com>
2014-05-23 14:37:29 +0100

    Add eligibility check

--- assets/prototype/javascripts/FormStorage.js
+++ assets/prototype/javascripts/FormStorage.js
@@ -5,7 +5,7 @@
     el: 'form',
 
     init: function () {
-      _.bindAll(this, 'render', 'saveField');
+      _.bindAll(this, 'render', 'onFieldChange', 'saveField', 'checkElig');
       this.cacheEls();
       this.bindEvents();
     },
@@ -13,15 +13,17 @@
     bindEvents: function () {
       moj.Events.on('render', this.render);
 
-      $(this.el).on('change keyup', 'input[type="radio"], input[type="checkbox"], input[type="text"], input[type="number"], textarea', this.saveField);
+      $(this.el).on('change keyup', 'input[type="radio"], input[type="checkbox"], input[type="text"], input[type="number"], textarea', this.onFieldChange);
     },
 
     cacheEls: function () {
-      this.FormData = sessionStorage.getItem("FormData") !== null ? JSON.parse(sessionStorage.getItem("FormData")) : {};
+      this.$form = $('form');
+      this.formData = sessionStorage.getItem('FormData') !== null ? JSON.parse(sessionStorage.getItem('FormData')) : {};
+      this.journey = sessionStorage.getItem('journey');
     },
 
     render: function () {
-      $.each(this.FormData, function(i, obj) {
+      $.each(this.formData, function(i, obj) {
         if (obj.type === "radio" || obj.type === "checkbox") {
           $('[name="' + obj.name + '"][value="' + obj.value + '"]').prop('checked', true);
         } else {
@@ -35,19 +37,54 @@
             .find('[required]').removeAttr('required');
         }
       });
+
       moj.Events.trigger('LabelSelect.render');
+
+      this.checkElig();
     },
 
-    saveField: function (e) {
-      var $el = $(e.target),
-          data = {};
+    onFieldChange: function (e) {
+      var $el = $(e.target);
 
-      data.name =  $el.attr('name');
-      data.value = $el.val();
-      data.type = $el.attr('type');
+      this.saveField($el);
+      this.checkElig();
+    },
 
-      this.FormData[$el.attr('name')] = data;
-      sessionStorage.setItem("FormData", JSON.stringify(this.FormData));
+    saveField: function (el) {
+      var data = {};
+
+      data.name =  el.attr('name');
+      data.value = el.val();
+      data.type = el.attr('type');
+      data.limit = el.data('fail-limit');
+
+      this.formData[el.attr('name')] = data;
+      sessionStorage.setItem('FormData', JSON.stringify(this.formData));
+    },
+
+    checkElig: function () {
+      var eligible = true;
+
+      $.each(this.formData, function(i, obj) {
+        if (obj.limit !== undefined && obj.value > obj.limit) {
+          eligible = false;
+        }
+      });
+
+      // Journey A
+      if (this.journey === 'A') {
+        if (!eligible) {
+          this.$form.attr('action', 'ineligible.html');
+        } else {
+          this.$form.attr('action', $('body').data('next-step'));
+        }
+      } 
+      // Journey B
+      else {
+        if (this.$form.attr('action') === 'eligible.html' && !eligible) {
+          this.$form.attr('action', 'ineligible.html');
+        }
+      }
     }
   };
 }());
