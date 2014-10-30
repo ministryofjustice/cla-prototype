8870c027a5d46df79870b5d612282c993af34a40
Tyom Semonov <inspiro@gmail.com>
2014-10-30 15:24:53 +0000

    Add selective sidebar

--- source/assets/javascripts/controllers.js
+++ source/assets/javascripts/controllers.js
@@ -27,7 +27,12 @@ app.controller('CheckerCtrl', function($scope, $state, $resource, storage, decis
     $scope.hasOtherBenefits = _.some(value, { name: 'none_of_above', value: true });
   });
 
-  $scope.$root.hasSidebar = false;
+
+  $scope.$root.sidebar = null;
+
+  if(!$state.params.stage.match(/^result/)) {
+    $scope.$root.sidebar = 'sidebar-reminder';
+  }
 
   $scope.submit = function(form) {
     $scope.submittedForms.push(form.$name.replace(/Form$/, ''));
